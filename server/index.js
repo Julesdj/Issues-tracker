f814061aff08055f2f6dabda4bb3d188cf3796ed
Kokutse Djoguenou <kokutse@Kokutses-MacBook-Pro.local>
2022-08-20 13:33:48 -0400

    Bug fixes

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@
 dotenv.config(); //Make sure this is at the top of your file otherwise your app can't access the .env file
 import mongoose from 'mongoose';
 import Express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import users from './controllers/user.js';
 import authn from './controllers/authn.js';
-import path from 'path';
 import tickets from './controllers/ticket.js';
 
 //Connection to MongoDB
@@ -19,11 +19,6 @@ mongoose
 //Variables
 const app = Express();
 
-//Controllers
-app.get('/', (req, res) => {
-    res.send('You have reached Tracker Inc. api');
-});
-
 //Middleware
 app.use(Express.json());
 app.use('/api/users', users); // add new user
@@ -35,6 +30,11 @@ const __dirname = path.resolve();
 app.use(Express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+});
+
+//Controllers
+app.get('/', (req, res) => {
+    res.send('You have reached Tracker Inc. api');
 });
 
 //Port
